62bbe3841c630cb93da988534e8b85f67381be8b
Ismoilkhon <ismoilkhon.bakhromov@mail.ru>
2023-02-22 23:39:48 +0500

    Implemented Guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,26 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        return this.#calculateAvarage()
     }
 
     lower() {
-
+        this.max = this.#calculateAvarage()
+        return this.max
     }
 
     greater() {
+        this.min = this.#calculateAvarage()
+        return this.min
+    }
 
+    #calculateAvarage() {
+        return Math.round((this.min + this.max) / 2)
     }
 }
 
